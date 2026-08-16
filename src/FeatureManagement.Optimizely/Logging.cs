dfb2e8d5994396d364d26c2c63261bf2e96af5bb
James Taranto <taranto.james@gmail.com>
2026-08-16 14:13:09 +0200

    fix: log decision with tag provider instead of LogProperties

--- src/FeatureManagement.Optimizely/Logging.cs
+++ src/FeatureManagement.Optimizely/Logging.cs
@@ -9,7 +9,20 @@ namespace TarantoJ.FeatureManagement.Optimizely
         internal static partial void LogDecision(
             this ILogger logger,
             string featureName,
-            [LogProperties] OptimizelyDecision decision
+            [TagProvider(typeof(DecisionTagProvider), nameof(DecisionTagProvider.Decision))]
+            OptimizelyDecision decision
         );
+    }
+
+    internal static class DecisionTagProvider
+    {
+        public static void Decision(ITagCollector tags, OptimizelyDecision decision)
+        {
+            tags.Add(nameof(decision.FlagKey), decision.FlagKey);
+            tags.Add(nameof(decision.Enabled), decision.Enabled);
+            tags.Add(nameof(decision.VariationKey), decision.VariationKey);
+            tags.Add(nameof(decision.RuleKey), decision.RuleKey);
+            tags.Add(nameof(decision.Reasons), decision.Reasons);
+        }
     }
 }
